dffd961816a92b1a10bbc9e109ea18de8a11ff69
MaksymSobotiuk <maxim.4.july.2001@gmail.com>
2024-05-24 12:40:31 +0300

    Step5

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,11 @@ const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
     setFeedback({ good: 0, neutral: 0, bad: 0 });
   };
 
- const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  
+  const positiveFeedbackPercentage = totalFeedback > 0
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
 
   return (
     <>
@@ -40,7 +44,7 @@ const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
       totalFeedback={totalFeedback}></Options>
 
       {totalFeedback > 0 ? (
-        <Feedback feedback={feedback} />
+        <Feedback feedback={feedback} totalFeedback={totalFeedback} positiveFeedbackPercentage={positiveFeedbackPercentage}/>
       ) : (
         <Notification message={"No feedback given yet."} />
       )}
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,12 +1,14 @@
 import css from './Feedback.module.css'
 
-export default function Feedback({feedback}) {
+export default function Feedback({feedback,totalFeedback, positiveFeedbackPercentage}) {
     
     return (
         <div>
               <p>Good: {feedback.good}</p>
             <p>Neutral: {feedback.neutral}</p>
             <p>Bad: {feedback.bad}</p>
+            <p>Total: {totalFeedback}</p>
+             <p>Positive: {positiveFeedbackPercentage}%</p>
     </div>
 )
 
