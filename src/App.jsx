1288e6bb66198db67163401dcdf8a5828f21679f
MaksymSobotiuk <maxim.4.july.2001@gmail.com>
2024-05-24 12:18:27 +0300

    step 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import Options from "./components/Options/Options"
 
 import Feedback from "./components/Feedback/Feedback"
 
-
+import { useState } from 'react';
 
 
 
 export default function App() {
 
-const feedback = {good: 0,
-	neutral: 0,
-	bad: 0}
+const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
+  const updateFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1
+    }));
+  };
 
-
-  
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
 
   return (
     <>
@@ -25,7 +30,9 @@ const feedback = {good: 0,
         text={"Please leave your feedback about our service by selecting one of the options below."}
       ></Description>
 
-      <Options btnTxt1={"Good"} btnTxt2={"Neutral"} btnTxt3={"Bad"} btnTxt4={"Reset"}></Options>
+      <Options btnTxt1={"Good"} btnTxt2={"Neutral"} btnTxt3={"Bad"} btnTxt4={"Reset"}
+          updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}></Options>
 
       <Feedback
         feedback={feedback}
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,17 +1,13 @@
 import css from './Options.module.css'
 
-export default function Options (props) {
-    
-    return (
-        <div>
-            <button type='button'>{props.btnTxt1}</button>
-            <button type='button'>{props.btnTxt2}</button>
-            <button type='button'>{props.btnTxt3}</button>
-            <button type='button'>{props.btnTxt4}</button>
+
+export default function Options({ btnTxt1, btnTxt2, btnTxt3, btnTxt4, updateFeedback, resetFeedback }) {
+  return (
+    <div>
+      <button type='button' onClick={() => updateFeedback('good')}>{btnTxt1}</button>
+      <button type='button' onClick={() => updateFeedback('neutral')}>{btnTxt2}</button>
+      <button type='button' onClick={() => updateFeedback('bad')}>{btnTxt3}</button>
+      <button type='button' onClick={resetFeedback}>{btnTxt4}</button>
     </div>
-
-
-)
-
-
+  );
 }
