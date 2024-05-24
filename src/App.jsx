a02e5e27268341235592501c43633a5ee90f9279
MaksymSobotiuk <maxim.4.july.2001@gmail.com>
2024-05-24 13:07:30 +0300

    Step6

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import Description from "./components/Description/Description"
-
-import Options from "./components/Options/Options"
-
-import Feedback from "./components/Feedback/Feedback"
-
+import React, { useState, useEffect } from 'react';
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
 
-import { useState } from 'react';
-
-
-
 export default function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem('feedback');
+    return savedFeedback ? JSON.parse(savedFeedback) : { good: 0, neutral: 0, bad: 0 };
+  });
 
-const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
 
   const updateFeedback = feedbackType => {
     setFeedback(prevFeedback => ({
@@ -26,7 +26,6 @@ const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
   };
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-  
   const positiveFeedbackPercentage = totalFeedback > 0
     ? Math.round((feedback.good / totalFeedback) * 100)
     : 0;
@@ -36,18 +35,23 @@ const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
       <Description
         title={"Sip Happens Café"}
         text={"Please leave your feedback about our service by selecting one of the options below."}
-      ></Description>
+      />
 
-      <Options btnTxt1={"Good"} btnTxt2={"Neutral"} btnTxt3={"Bad"} btnTxt4={"Reset"}
-          updateFeedback={updateFeedback}
+      <Options
+        btnTxt1={"Good"}
+        btnTxt2={"Neutral"}
+        btnTxt3={"Bad"}
+        btnTxt4={"Reset"}
+        updateFeedback={updateFeedback}
         resetFeedback={resetFeedback}
-      totalFeedback={totalFeedback}></Options>
+        totalFeedback={totalFeedback}
+      />
 
       {totalFeedback > 0 ? (
-        <Feedback feedback={feedback} totalFeedback={totalFeedback} positiveFeedbackPercentage={positiveFeedbackPercentage}/>
+        <Feedback feedback={feedback} totalFeedback={totalFeedback} positiveFeedbackPercentage={positiveFeedbackPercentage} />
       ) : (
         <Notification message={"No feedback given yet."} />
       )}
     </>
-  )
+  );
 }
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,3 +1,5 @@
+import css from './Notifacation.module.css'
+
 export default function Notification({ message }) {
   return (
     <p>{message}</p>
